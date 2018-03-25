e0a0b9e8f0ec0b72b1c86db2e71ed22e66222557
angelbanderasl <36874099+angelbanderasl@users.noreply.github.com>
2018-03-24 22:07:05 -0600

    Update shadow.sh

--- shadow.sh
+++ shadow.sh
@@ -1,9 +1,9 @@
 echo Introduce tu Ip :
 read Ipaddress
-sed -i "s,Ipaddress,$Ipaddress,g" /etc/shadowsocks.json
+sed -i "s,546345,$Ipaddress,g" /etc/shadowsocks.json
 
 echo Introduce tu Contraseña :
-read -p pal
-sed -i "s,45634563,$pal,g" /etc/shadowsocks.json
+read pass
+sed -i 's,"",$pass,g' /etc/shadowsocks.json
 cat /etc/shadowsocks.json
 
