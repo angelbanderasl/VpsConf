f40fca829508ff5a1fcd38bd45be1b40c6af55b5
angelbanderasl <36874099+angelbanderasl@users.noreply.github.com>
2018-03-24 22:00:58 -0600

    Update shadow.sh

--- shadow.sh
+++ shadow.sh
@@ -1,11 +1,10 @@
 echo Introduce tu Ip :
-read -p ": " Ipad
+read Ipad
 Ipaddress="$Ipad"
-sed -i "s/101010/Ipaddress/g" /etc/shadowsocks.json
+sed -i "s/Ipaddress/$Ipaddress/g" /etc/shadowsocks.json
 
 echo Introduce tu Contraseña :
-read -p ": " pal
-pass="$pal"
-sed -i "s,0000,$pass,g" /etc/shadowsocks.json
+read -p pal
+sed -i "s,0000,$pal,g" /etc/shadowsocks.json
 cat /etc/shadowsocks.json
 
